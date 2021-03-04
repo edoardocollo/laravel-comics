04f9e6a563a28065de07f30c1a4ffcbaa102f534
edoardo <collo>
2021-03-04 12:19:13 +0100

    add some style

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Comic;
+class PageController extends Controller
+{
+    public function index(){
+      $comics = Comic::all();
+      return view('guest.welcome',compact('comics'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-});
+Route::get('/', 'Guest\PageController@index');
 
 Auth::routes(['register' => false]);
 
