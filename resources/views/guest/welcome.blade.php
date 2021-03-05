8c7f53ce8901d896fed5e54bcd536660a0b5b591
edoardo <collo>
2021-03-05 14:16:39 +0100

    general style review

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -7,15 +7,13 @@
     <h1>comics & graphic novels</h1>
     <div class="carousel">
       @foreach($comics as $comic)
-      <div class="carousel_comic_card">
-        <img src="storage/{{$comic->img}}" alt="">
-        <span>{{$comic->titolo}}</span>
-        <span>{{$comic->disponibilita?'available now':'not available'}}</span>
-        <form action="{{route('comics', $comic->id )}}" method="get">
-          @csrf
-         <button type="submit" class="btn btn-success">show</button>
-        </form>
-      </div>
+             <div class="carousel_comic_card">
+               <a href="{{route('comics', $comic->id )}}">
+                 <img src="storage/{{$comic->img}}" alt="">
+               </a>
+               <span>{{$comic->titolo}}</span>
+               <span>{{$comic->disponibilita?'available now':'not available'}}</span>
+             </div>
       @endforeach
     </div>
   </section>
@@ -28,9 +26,21 @@
       @foreach($musts as $must)
       <div class="carousel_must_card">
         <img src="storage/{{$must->img}}" alt="">
-        <span>{{$must->sottotitolo}}</span>
+        <span class="subtitle">{{$must->sottotitolo}}</span>
         <span>{{$must->titolo}}</span>
         <p>{{$must->contenuto}}</p>
+      </div>
+      @endforeach
+    </div>
+  </section>
+
+  <section id="current_series">
+    <h1>current series</h1>
+    <div class="carousel">
+      @foreach($series as $serie)
+      <div class="carousel_serie_card">
+        <img src="storage/{{$serie->img}}" alt="">
+        <p>{{$serie->serie}}</p>
       </div>
       @endforeach
     </div>
--- resources/views/layouts/guest_app.blade.php
+++ resources/views/layouts/guest_app.blade.php
@@ -22,7 +22,26 @@
 </head>
 <body>
   <div id="main_container">
-    <nav id="area_nav" class="col-xl-12 col-md-12 col-xs-12"></nav>
+    <nav id="area_nav" class="col-xl-12 col-md-12 col-xs-12">
+      <div class="area_nav_wrap">
+        <div class="logo_box">
+          <img src="{{asset('storage/posts_img/logo0.png')}}" alt="">
+        </div>
+        <div class="logo_box">
+          <img src="{{asset('storage/posts_img/logo1.png')}}" alt="">
+        </div>
+        <div class="logo_box">
+          <img src="{{asset('storage/posts_img/logo2.png')}}" alt="">
+        </div>
+        <div class="logo_box">
+          <img src="{{asset('storage/posts_img/logo3.png')}}" alt="">
+        </div>
+        <div class="logo_box">
+          <img src="{{asset('storage/posts_img/logo4.png')}}" alt="">
+        </div>
+      </div>
+
+    </nav>
     <nav id="main_nav">
       <div class="main_nav_wrap">
         <img src="{{asset('storage/posts_img/logo.png')}}" alt="">
@@ -44,10 +63,22 @@
 
   <nav id="services_nav">
     <div class="services_wrap">
-      <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-bar-DCUI.svg" alt="">
-      <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-bar-DCshop.svg" alt="">
-      <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-bar-subscription.svg" alt="">
-      <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-bar-locator.svg" alt="">
+      <div class="service_box">
+        <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-bar-DCUI.svg" alt="">
+        <span>digital comics</span>
+      </div>
+      <div class="service_box">
+        <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-bar-DCshop.svg" alt="">
+        <span>dc merchandise</span>
+      </div>
+      <div class="service_box">
+        <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-bar-subscription.svg" alt="">
+        <span>print subscriptions</span>
+      </div>
+      <div class="service_box">
+        <img src="https://www.dccomics.com/sites/all/themes/dc_comics_bp/images/buy-bar-locator.svg" alt="">
+        <span>comic shop locator</span>
+      </div>
     </div>
   </nav>
 
@@ -58,17 +89,12 @@
   <div class="footer_wrap">
     <div class="footer_half">
       <div class="footer_column">
+        <h3>dc comics</h3>
         <ul>
           <li>lorem</li>
           <li>lorem</li>
           <li>lorem</li>
           <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-          <li>lorem</li>
-        </ul>
-        <ul>
           <li>lorem</li>
           <li>lorem</li>
           <li>lorem</li>
@@ -76,6 +102,7 @@
         </ul>
       </div>
       <div class="footer_column">
+        <h3>dc</h3>
         <ul>
           <li>lorem</li>
           <li>lorem</li>
@@ -89,6 +116,7 @@
 
       </div>
       <div class="footer_column">
+        <h3>sites</h3>
         <ul>
           <li>lorem</li>
           <li>lorem</li>
@@ -104,17 +132,31 @@
       <div class="footer_column">
 
       </div>
+      <p>
+        All Site Content TM and © 2020 DC Entertainment, unless otherwise noted here. All rights reserved.
+        Cookies Settings
+      </p>
     </div>
 
 
-    <div class="footer_half">
-
+    <div class="footer_half half_right">
     </div>
 
-
+    <img id="logo_background" src="{{asset('storage/posts_img/logo-background-v2.png')}}" alt="">
   </div>
   <div class="sub_footer">
-
+    <div class="sub_footer_wrap">
+      <div class="sub_footer_left">
+        <a href="#">sign up now</a>
+      </div>
+      <div class="sub_footer_right">
+        <img src="{{asset('storage/posts_img/footer-facebook.png')}}" alt="">
+        <img src="{{asset('storage/posts_img/footer-periscope.png')}}" alt="">
+        <img src="{{asset('storage/posts_img/footer-pinterest.png')}}" alt="">
+        <img src="{{asset('storage/posts_img/footer-twitter.png')}}" alt="">
+        <img src="{{asset('storage/posts_img/footer-youtube.png')}}" alt="">
+      </div>
+    </div>
   </div>
 
 </footer>
